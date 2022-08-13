df188890f225dc952494b6fc5dad1a9a5fc9c507
Cucchi01 <cucchi01centr@gmail.com>
2022-08-13 15:56:29 +0200

    Working model
    Finished the changes from the model used for the "adult" dataset for
    creating a working version. The results got a score of 0.77990 on Kaggle

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import os.path
 import pandas as pd
 import matplotlib.pyplot as plt
 
-N_EPOCHS = 5000
+N_EPOCHS = 20000
 # With the value at false first it checks if there is already a model to load
-FORCE_GENERATION_NEW_MODEL = True
+FORCE_GENERATION_NEW_MODEL = False
 LR = 1e-3
 PATH_DATA = "data/"
 
@@ -25,64 +25,84 @@ class Model(nn.Module):
         hidden_t = self.hidden_layer(input)
         activated_t = self.hidden_activation(hidden_t)
         output_t = self.output_linear(activated_t)
-        return self.setTo1Or0(output_t, threshold= 0.5)
-    
-    def setTo1Or0(self, data_t, threshold=0.5):
-        for i in range(data_t.shape[0]):
-            if data_t[i] <= threshold:
-                data_t[i] = 0
-            else:
-                data_t[i] = 1
-        return data_t
-
+        return output_t
 
 
 def main():
 
-    (TensorInputForTraining, TensorOutputForTraining) = generateTensorForTraining()
+    (TensorInputForTraining, TensorOutputForTraining,
+     TensorInputForTest, passengerId) = generateTensorForTraining()
 
-    # model = getModel(TensorInputForTraining, TensorOutputForTraining, TensorInputForTest,
-    #                  TensorOutputForTest, nameModelFile='model.pth')
+    model = getModel(TensorInputForTraining,
+                     TensorOutputForTraining, nameModelFile='model.pth')
 
-    # outputStatsModel(model, TensorInputForTest,
-    #                  TensorOutputForTest, saveHidWeightsToFileFlag=True)
+    res = generateOutputTest(TensorInputForTest, model)
+    saveResults(res, passengerId)
 
 
 def generateTensorForTraining():
     trainData: pd.DataFrame = loadData("trainEncoded.csv", "train.csv")
-    #testData: pd.DataFrame = loadData("testEncoded.npy", "test.csv")
-    
+    testData: pd.DataFrame = loadData("testEncoded.csv", "test.csv")
+
+    passengerId = testData.loc[:, "PassengerId"]
+
     resultTraining = trainData.loc[:, "Survived"]
-    inputTraining = trainData.drop("Survived", axis= 1)
-    
-    TensorInputForTraining = torch.from_numpy(inputTraining.to_numpy())
-    TensorResForTraining = torch.from_numpy(resultTraining.to_numpy()) 
+    inputTraining = trainData.drop("Survived", axis=1)
+    inputTraining = inputTraining.drop("PassengerId", axis=1)
+    testData = testData.drop("PassengerId", axis=1)
+
+    TensorInputForTraining = torch.from_numpy(
+        inputTraining.to_numpy(dtype=np.float32, na_value=0))
+    TensorResForTraining = torch.from_numpy(
+        resultTraining.to_numpy(dtype=np.float32, na_value=0))
+    TensorInputForTest = torch.from_numpy(
+        testData.to_numpy(dtype=np.float32, na_value=0))
 
     normalizeTensor(TensorInputForTraining)
-    print(TensorInputForTraining)
+    normalizeTensor(TensorInputForTest)
 
     TensorResForTraining = TensorResForTraining.unsqueeze(1)
-    print(TensorResForTraining)
 
-    return (TensorInputForTraining, TensorResForTraining)
+    return (TensorInputForTraining, TensorResForTraining, TensorInputForTest, passengerId)
 
 
-def getModel(TensorInputForTraining, TensorOutputForTraining, TensorInputForTest,
-             TensorOutputForTest, nameModelFile):
-    if os.path.exists("model/"+nameModelFile) and FORCE_GENERATION_NEW_MODEL == False:
+def getModel(TensorInputForTraining, TensorOutputForTraining, nameModelFile):
+    if os.path.exists(PATH_DATA+nameModelFile) and FORCE_GENERATION_NEW_MODEL == False:
         model = Model(TensorInputForTraining.shape[1])
-        model.load_state_dict(torch.load("model/"+nameModelFile))
+        model.load_state_dict(torch.load(PATH_DATA+nameModelFile))
     else:
-        model = generateModel(TensorInputForTraining, TensorOutputForTraining, TensorInputForTest,
-                              TensorOutputForTest)
-        torch.save(model.state_dict(), 'model/'+nameModelFile)
+        model = generateModel(TensorInputForTraining, TensorOutputForTraining)
+        torch.save(model.state_dict(), PATH_DATA+nameModelFile)
 
     return model
 
 
+def generateOutputTest(TensorInputForTest, model):
+    t_p_test: torch.Tensor = model(TensorInputForTest)
+    res: torch.Tensor = setTo1Or0(t_p_test, 0.50)
+    return res
+
+def saveResults(res, passengerId):
+    resDf = pd.DataFrame(res.detach().numpy())
+    resDf= resDf.astype(int)
+
+    resDf= resDf.set_axis(["Survived"], axis=1)
+    resDf["PassengerId"] = passengerId
+    resDf.to_csv(PATH_DATA+"results.csv", index=False, columns=["PassengerId", "Survived"])
+
+def setTo1Or0(data_t, threshold=0.5):
+    for i in range(data_t.shape[0]):
+        if data_t[i] <= threshold:
+            data_t[i] = 0
+        else:
+            data_t[i] = 1
+    return data_t
+
+
 def loadData(namePdFile, nameFileData):
     if os.path.exists(PATH_DATA+namePdFile):
-        data: pd.DataFrame =  pd.read_csv(PATH_DATA+namePdFile, delimiter=',', header=0)
+        data: pd.DataFrame = pd.read_csv(
+            PATH_DATA+namePdFile, delimiter=',', header=0)
     else:
         data: pd.DataFrame = loadDataFromFile(nameFileData)
         data.to_csv(PATH_DATA+namePdFile, index=False)
@@ -93,18 +113,17 @@ def normalizeTensor(adultData):
     n_channels = adultData.shape[1]
     for i in range(0, n_channels):
         max = torch.max(adultData[:, i])
-        if max > 1e-8:
+        if max > 1e-6:
             min = torch.min(adultData[:, i])
             adultData[:, i] = (adultData[:, i] - min) / max
 
 
-def generateModel(TensorInputForTraining, TensorOutputForTraining, TensorInputForTest,
-                  TensorOutputForTest):
+def generateModel(TensorInputForTraining, TensorOutputForTraining):
     model = Model(TensorInputForTraining.shape[1])
     optimizer = optim.Adam(model.parameters(), lr=LR)
 
     trainingLoop(n_epochs=N_EPOCHS, optimizer=optimizer, model=model, loss_fn=nn.MSELoss(
-    ), TensorInputForTraining=TensorInputForTraining, TensorInputForTest=TensorInputForTest,  TensorOutputForTraining=TensorOutputForTraining, TensorOutputForTest=TensorOutputForTest)
+    ), TensorInputForTraining=TensorInputForTraining, TensorOutputForTraining=TensorOutputForTraining)
 
     return model
 
@@ -182,75 +201,42 @@ def saveHiddenWeightsToFile(weightPerFeature):
 def loadDataFromFile(nameFile):
     df = pd.read_csv(PATH_DATA+nameFile, header=0, quotechar='"')
     df = df.drop("Name", axis=1)
+    df = df.drop("Ticket", axis=1)
+    df = df.drop("Fare", axis=1)
+    df = df.drop("Cabin", axis=1)
     df = pd.get_dummies(df)
 
     return df
 
 
-def mapWords(adultData):
-    education2Int = {
-        b"?": b"0",
-        b"Preschool": b"1",
-        b"1st-4th": b"2",
-        b"5th-6th": b"3",
-        b"7th-8th": b"4",
-        b"9th": b"5",
-        b"10th": b"6",
-        b"11th": b"7",
-        b"12th": b"8",
-        b"HS-grad": b"9",
-        b"Prof-school": b"10",
-        b"Assoc-acdm": b"11",
-        b"Assoc-voc": b"12",
-        b"Some-college": b"12",
-        b"Bachelors": b"14",
-        b"Masters": b"15",
-        b"Doctorate": b"16",
-    }
-    mapIncome2Int = {
-        b"<=50K": b"0",
-        b">50K": b"1"
-    }
-
-    for row in adultData:
-        row[3] = education2Int[row[3]]
-        row[14] = mapIncome2Int[row[14]]
-
-    return adultData
-
-
-def trainingLoop(n_epochs, optimizer, model, loss_fn, TensorInputForTraining, TensorInputForTest, TensorOutputForTraining, TensorOutputForTest):
-    results = np.array([["Epoch", "Training Loss", "Test Loss"]])
+def trainingLoop(n_epochs, optimizer, model, loss_fn, TensorInputForTraining, TensorOutputForTraining):
+    results = np.array([["Epoch", "Training Loss"]])
     for epoch in range(1, n_epochs+1):
         t_p_train = model(TensorInputForTraining)
         loss_train = loss_fn(t_p_train, TensorOutputForTraining)
-
-        t_p_val = model(TensorInputForTest)
-        loss_val = loss_fn(t_p_val, TensorOutputForTest)
 
         optimizer.zero_grad()
         loss_train.backward()
         optimizer.step()
         results = np.append(
-            results, [[epoch, float(loss_train), float(loss_val)]], axis=0)
+            results, [[epoch, float(loss_train)]], axis=0)
 
         if epoch == 1 or epoch % 100 == 0:
-            print("Epoch {}, Training loss {}, Validation loss {},".format(
-                epoch, float(loss_train), float(loss_val)))
+            print("Epoch {}, Training loss {}".format(
+                epoch, float(loss_train)))
 
     printGraphTrainingResults(results, n_epochs)
 
 
 def printGraphTrainingResults(results, n_epochs):
     # n_epochs+1 because there are the names of the columns in the first row
-    results.reshape(n_epochs+1, 3)
+    results.reshape(n_epochs+1, 2)
     index = results[1:, 0]
     data = results[1:, 1:]
     columns = results[0, 1:]
     resultDF = pd.DataFrame(
         data=data, index=index, columns=columns)
     resultDF['Training Loss'] = resultDF['Training Loss'].astype(float)
-    resultDF['Test Loss'] = resultDF['Test Loss'].astype(float)
     resultDF.plot()
     plt.show()
 
