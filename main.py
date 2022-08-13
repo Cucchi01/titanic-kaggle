a064bfdcbe9bdaea1f9413c192cb8b1a1116bc0c
Cucchi01 <cucchi01centr@gmail.com>
2022-08-13 16:00:12 +0200

    Removed not used function

--- main.py
+++ main.py
@@ -82,13 +82,6 @@ def generateOutputTest(TensorInputForTest, model):
     res: torch.Tensor = setTo1Or0(t_p_test, 0.50)
     return res
 
-def saveResults(res, passengerId):
-    resDf = pd.DataFrame(res.detach().numpy())
-    resDf= resDf.astype(int)
-
-    resDf= resDf.set_axis(["Survived"], axis=1)
-    resDf["PassengerId"] = passengerId
-    resDf.to_csv(PATH_DATA+"results.csv", index=False, columns=["PassengerId", "Survived"])
 
 def setTo1Or0(data_t, threshold=0.5):
     for i in range(data_t.shape[0]):
@@ -97,6 +90,16 @@ def setTo1Or0(data_t, threshold=0.5):
         else:
             data_t[i] = 1
     return data_t
+
+
+def saveResults(res, passengerId):
+    resDf = pd.DataFrame(res.detach().numpy())
+    resDf = resDf.astype(int)
+
+    resDf = resDf.set_axis(["Survived"], axis=1)
+    resDf["PassengerId"] = passengerId
+    resDf.to_csv(PATH_DATA+"results.csv", index=False,
+                 columns=["PassengerId", "Survived"])
 
 
 def loadData(namePdFile, nameFileData):
@@ -126,76 +129,6 @@ def generateModel(TensorInputForTraining, TensorOutputForTraining):
     ), TensorInputForTraining=TensorInputForTraining, TensorOutputForTraining=TensorOutputForTraining)
 
     return model
-
-
-def outputStatsModel(model, TensorInputForTest, TensorOutputForTest, flagPrintTensor=False, saveHidWeightsToFileFlag=True):
-    if flagPrintTensor:
-        print('Actual answer', TensorOutputForTest)
-        print('Ouput Model:', model(TensorInputForTest))
-
-    outputTensor = model(TensorInputForTest)
-
-    # set output to 0 if it is lower than 0.5 or 1. They are the only
-    # two possible value for that domain, the income can only be lower(0) or greater(1) of 50k
-    for i in range(outputTensor.shape[0]):
-        if outputTensor[i] <= 0.5:
-            outputTensor[i] = 0
-        else:
-            outputTensor[i] = 1
-
-    if flagPrintTensor:
-        print('Ouput model ', outputTensor)
-
-    output = list(zip(outputTensor, TensorOutputForTest))
-    over50KOutput = []
-    under50KOutput = []
-    numDiffUnder = 0
-    numDiffOver = 0
-    for singleOutput in output:
-        if singleOutput[1] == 1:
-            over50KOutput.append(singleOutput)
-            if singleOutput[0] == 0:
-                numDiffOver += 1
-        else:
-            under50KOutput.append(singleOutput)
-            if singleOutput[0] == 1:
-                numDiffUnder += 1
-
-    diffTensor = outputTensor-TensorOutputForTest
-    diffTensor = torch.abs(diffTensor)
-    numWrongPrediction = int(torch.sum(diffTensor))
-    numPredictions = int(outputTensor.shape[0])
-
-    print("Number of predictions: ", numPredictions)
-    print("Number of correct predictions: ", numPredictions-numWrongPrediction)
-    print("Number of wrong predictions: ", numWrongPrediction)
-    print("Percentage of correct predictions: ",
-          (numPredictions-numWrongPrediction)/numPredictions*100)
-    print("Percentage of correct predictions over 50K: ",
-          (1 - numDiffOver/len(over50KOutput))*100)
-    print("Percentage of correct predictions under 50K: ",
-          (1 - numDiffUnder/len(under50KOutput))*100)
-
-    listFeature = hE.getListFeatureAfterHotEnc()
-    # remove "income" from the list
-    listFeature.pop()
-    hiddenLayerWeights = model.hidden_layer.weight.data.tolist()[0]
-    weightPerFeature = list(zip(listFeature, hiddenLayerWeights))
-    weightPerFeature.sort(key=lambda row: row[1], reverse=True)
-    if saveHidWeightsToFileFlag:
-        saveHiddenWeightsToFile(weightPerFeature)
-
-
-def saveHiddenWeightsToFile(weightPerFeature):
-    try:
-        fw = open("results/weightPerFeature.txt", "w")
-        for row in weightPerFeature:
-            fw.write("Feature: %-30s Weight: %3.5f\n" %
-                     (row[0].decode('UTF-8'), row[1]))
-
-        fw.close()
-    except IOError:
-        print("Error writing the weights of the hidden layer to file")
 
 
 def loadDataFromFile(nameFile):
